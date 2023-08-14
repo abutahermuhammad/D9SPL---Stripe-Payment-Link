b80354ee8b2837294175733b0d1f310d7fe2d9e4
Abu Taher Muhammad <abut1081@gmail.com>
2023-08-14 20:23:46 +0600

    `Activation.php` updated

--- inc/Base/Activate.php
+++ inc/Base/Activate.php
@@ -12,4 +12,18 @@ class Activate
     {
         flush_rewrite_rules();
     }
+
+    funciton d9spl_activate_plugin() {
+        if (version_compare(get_bloginfo('version'), '5.0', '<')) {
+            wp_die(__('You must update WordPress to use this plugin.', 'd9-spl'));
+        }
+    
+        $installed_version = get_option('d9spl_version');
+    
+        if (!$installed_version) {
+            update_option('d9spl_version', D9SPL_VERSION);
+        }
+    
+        update_option('d9spl_version', D9SPL_VERSION);
+    }
 }
