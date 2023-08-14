16f5dd429614308c6358b548145aac957feaa8d4
Abu Taher Muhammad <abut1081@gmail.com>
2023-08-14 18:42:31 +0600

    SettingsLiks edited

--- inc/Base/SettingsLinks.php
+++ inc/Base/SettingsLinks.php
@@ -11,16 +11,10 @@ if (!defined('ABSPATH')) exit; // Exit if accessed directly
 
 class SettingsLinks
 {
-    protected $plugin;
-
-    public function __construct()
-    {
-        $this->plugin = D9SPL_PLUGIN_NAME;
-    }
 
     public function register()
     {
-        add_filter("plugin_action_links_$this->plugin", array($this, "settings_link"));
+        add_filter("plugin_action_links_$this->plugin" . D9SPL_PLUGIN_NAME, array($this, "settings_link"));
     }
 
     public function settings_link($links)
